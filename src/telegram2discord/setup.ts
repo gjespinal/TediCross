87397f3e1f55efc3f4facbee225c2e3c707e2841
gjespinal <guillermoespinal@gmail.com>
2025-02-07 10:25:45 -0400

    Update setup.ts
    Signed-off-by: gjespinal <guillermoespinal@gmail.com>

--- src/telegram2discord/setup.ts
+++ src/telegram2discord/setup.ts
@@ -167,7 +167,6 @@ export function setup(
 			tgBot.use(middlewares.addMessageObj);
 			tgBot.use(middlewares.addMessageId);
 			tgBot.use(middlewares.addBridgesToContext);
-			/*tgBot.use(middlewares.informThisIsPrivateBot);*/
 			tgBot.use(middlewares.removeD2TBridges);
 			//@ts-ignore telegram expacts a second parameter
 			//tgBot.command(middlewares.removeBridgesIgnoringCommands);
