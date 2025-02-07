a5cf36e4ff6be38b3e7629bc08aa4e76cd013449
gjespinal <guillermoespinal@gmail.com>
2025-02-07 13:11:48 -0400

    Update setup.ts
    Signed-off-by: gjespinal <guillermoespinal@gmail.com>

--- src/telegram2discord/setup.ts
+++ src/telegram2discord/setup.ts
@@ -200,8 +200,8 @@ export function setup(
 			        return;
 			    }
 			
-			    if (message.message_thread_id !== 86) {
-			        console.log(`❌ Ignorando mensaje de tópico ${message.message_thread_id}, solo permitimos 86`);
+			    if (message.message_thread_id !== 9167) {
+			        console.log(`❌ Ignorando mensaje de tópico ${message.message_thread_id}, solo permitimos 9167`);
 			        return;
 			    }
 			
